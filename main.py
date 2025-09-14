cf7ba6776f836e7cd832adfc4b423e64e3d9a473
jeffschoe <ratacame@protonmail.com>
2025-09-13 23:35:44 -0400

    CH2L1: Draw Lines.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 
 def main():
     win = Window(800, 600)
+
+    p1 = Point(50, 50)
+    p2 = Point(300, 500)
+    line1 = Line(p1, p2)
+    win.draw_line(line1, "red")
+
+    p3 = Point(100, 200)
+    p4 = Point(500, 400)
+    line2 = Line(p3, p4)
+    win.draw_line(line2, "blue")
+
     win.wait_for_close()
 
 
